148bfec2672f536b692ea8ad3bbcaf5b0d75c4ed
Pedro19ar <paristizabala@unal.edu.co>
2026-05-08 20:45:16 -0500

    feat: configurar backend s3 para tfstate y agregar workflow de destroy

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,11 @@
-    provider "aws" {
-region = "us-east-1"
+terraform {
+  backend "s3" {
+    bucket = "aws-tfstate-pedro19ar-2026"
+    key    = "novalink-aws-ai-agent/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
